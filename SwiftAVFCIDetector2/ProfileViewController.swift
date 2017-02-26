014f3b9d3f1ef15ff8f6b5ec816ffc12fce2a7b1
吉澤実 <yoshizawaminoru@yoshizawaminoru-no-MacBook-Pro.local>
2017-02-26 17:53:34 +0900

    aaa

---
+++ SwiftAVFCIDetector2/ProfileViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileViewController.swift
+//  SwiftAVFCIDetector2
+//
+//  Created by 吉澤実 on H29/02/26.
+//  Copyright © 平成29年 Yoshihisa Nitta. All rights reserved.
+//
+
+import Foundation
